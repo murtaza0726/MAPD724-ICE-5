4cd61079552fc9f454ba52c451fbc9d76025dbbe
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2023-02-19 22:57:08 -0500

    ICE-5

---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+  public Boundary boundary;
+  public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Move();
+      CheckBounds();
+    }
+
+  void Move()
+  {
+    foreach (var touch in Input.touches)
+    {
+      transform.position = new Vector2(camera.ScreenToWorldPoint(touch.position).x, -4.0f);
+    }
+  }
+
+  void CheckBounds()
+  {
+    // Right Bounds
+    if(transform.position.x > boundary.right)
+    {
+      transform.position = new Vector2(boundary.right, -4.0f);
+    }
+
+    // Left Bounds
+    if (transform.position.x < boundary.left)
+    {
+      transform.position = new Vector2(boundary.left, -4.0f);
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("Cloud"))
+    {
+        // Play the thunder sound
+        other.gameObject.GetComponent<AudioSource>().Play();
+
+        // Take Damage / Lose Health
+    }
+
+    if (other.gameObject.CompareTag("Island"))
+    {
+        // Play the yay sound
+        other.gameObject.GetComponent<AudioSource>().Play();
+        
+        // Gain points
+    }
+  }
+}
